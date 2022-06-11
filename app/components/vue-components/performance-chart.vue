b9c2c35634b4e00c380472909f7f10acfe97b808
M7MD_SAMI <samy.ashoor@gmail.com>
2022-06-11 15:11:09 +0200

    Install Axios and call chart endpoint

--- app/components/vue-components/performance-chart.vue
+++ app/components/vue-components/performance-chart.vue
@@ -16,6 +16,7 @@ import {
   VisualMapComponent,
 } from "echarts/components";
 import VChart from "vue-echarts";
+import axios from 'axios';
 
 use([
   CanvasRenderer,
@@ -35,36 +36,7 @@ export default {
 
   data() {
     return {
-      chartData: [
-        {
-          date_ms: 1641772800000,
-          performance: 0.2,
-        },
-        {
-          date_ms: 1641859200000,
-          performance: 0.33,
-        },
-        {
-          date_ms: 1641945600000,
-          performance: 0.53,
-        },
-        {
-          date_ms: 1642032000000,
-          performance: 0.31,
-        },
-        {
-          date_ms: 1642118400000,
-          performance: 0.65,
-        },
-        {
-          date_ms: 1642204800000,
-          performance: 0.88,
-        },
-        {
-          date_ms: 1642291200000,
-          performance: 0.07,
-        },
-      ],
+      chartData: [],
     };
   },
 
@@ -143,6 +115,17 @@ export default {
     formatDate(dateInMs) {
       return moment(dateInMs).format("DD MMM YYYY");
     },
+    getChartData() {
+      axios.get(`https://fe-task.getsandbox.com/performance`)
+        .then(res => {
+          console.log(res.data)
+          this.chartData = [...this.chartData, ...res.data]
+        })
+    }
   },
+
+  mounted() {
+    this.getChartData();
+  }
 };
 </script>
--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
     new ESLintPlugin({
       failOnError: true,
       emitWarning: false,
-      outputReport: true
+      outputReport: true,
     }),
     new VueLoaderPlugin(),
   ],
