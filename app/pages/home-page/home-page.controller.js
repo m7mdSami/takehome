92aa09826fbfa4adf7024ffdfccaa8cb7286941a
M7MD_SAMI <samy.ashoor@gmail.com>
2022-06-13 13:36:34 +0200

    Implement the logic of the load more button

--- app/pages/home-page/home-page.controller.js
+++ app/pages/home-page/home-page.controller.js
@@ -7,17 +7,24 @@ function homePageController(Employees, $location) {
   const { filter } = $location.search();
   homePageVm.employees = [];
   homePageVm.searchValue = filter;
+  homePageVm.current_page = 1;
+
+  const getEmployees = (page) => {
+    Employees.getEmployees(page)
+      .then(({ data }) => {
+        console.log(data);
+        homePageVm.current_page = data.current_page;
+        homePageVm.employees = homePageVm.employees.concat(data.employees);
+      });
+  };
 
   activate();
 
   function activate() {
-    Employees.getEmployees()
-      .then(({ data }) => {
-        homePageVm.employees = homePageVm.employees.concat(data.employees);
-      });
+    getEmployees(homePageVm.current_page);
   }
 
-  homePageVm.filter = function (value) {
+  homePageVm.filter = (value) => {
     if (value && value !== '') {
       $location.search('filter', value);
       homePageVm.searchValue = value;
@@ -25,5 +32,10 @@ function homePageController(Employees, $location) {
       $location.search('filter', null);
       homePageVm.searchValue = '';
     }
+  };
+
+  homePageVm.loadMore = (page) => {
+    console.log(999);
+    getEmployees(page);
   };
 }
--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,15 @@
 const { defineConfig } = require('cypress');
+const webpackConfig = require('./webpack.config.js');
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:8082',
+  },
+  component: {
+    devServer: {
+      framework: 'angular',
+      bundler: 'webpack',
+      webpackConfig,
+    },
   },
 });
